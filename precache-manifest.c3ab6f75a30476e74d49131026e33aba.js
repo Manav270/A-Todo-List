eb12eaad2151a4ff23adb747a86cc820a8fd96fe
Manav270 <manavarora7006@gmail.com>
2022-08-16 15:47:20 +0530

    Updates

---
+++ precache-manifest.c3ab6f75a30476e74d49131026e33aba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f3b4575e4f7c6ec8ef302d64367cd3",
+    "url": "/A-Todo-List/index.html"
+  },
+  {
+    "revision": "6a019708122ef2195904",
+    "url": "/A-Todo-List/static/js/2.fd03c0c9.chunk.js"
+  },
+  {
+    "revision": "72b588ba9733bb2fad8f",
+    "url": "/A-Todo-List/static/js/main.e44964ef.chunk.js"
+  },
+  {
+    "revision": "08cc0f32fcfd8b3f7ad9",
+    "url": "/A-Todo-List/static/js/runtime-main.ebe5eee3.js"
+  }
+]);
